c6369547d01bff4dcd129b23d2d58e1c4e7dbe3c
csn <806572349@qq.com>
2018-09-17 14:04:11 +0800

    自定义组件过滤规则

--- src/main/java/com/nemo/sj/spring/config/MainConfig.java
+++ src/main/java/com/nemo/sj/spring/config/MainConfig.java
@@ -1,6 +1,7 @@
 package com.nemo.sj.spring.config;
 
 import com.nemo.sj.spring.bean.Persion;
+import com.nemo.sj.spring.service.BookService;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -13,13 +14,24 @@ import org.springframework.stereotype.Repository;
 
 @ComponentScans(value = {
         @ComponentScan(value = "com.nemo.sj",
-                includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class,Repository.class})
+                includeFilters ={
+//                @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class,Repository.class}),
+//                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {BookService.class}),
+                        @ComponentScan.Filter(type = FilterType.CUSTOM,classes = {MyTypeFiler.class})
+
+                }
                 ,useDefaultFilters = false
         )
 })
 //value c指定扫描包
 //excludeFilters  Filter[]排除 某些
 //includeFilters  Filter[]扫描的时候只需要包含哪些组件
+
+//FilterType.ANNOTATION  按照注解方式
+//FilterType.ASSIGNABLE_TYPE  按照给定的类型
+//FilterType.ASPECTJ   使用ASPECTJ表达式
+//FilterType.REGEX  使用正则指定
+//FilterType.CUSTOM 自定义过滤
 public class MainConfig {
     //  默认id是用方法名作为id
     @Bean("person")
---
+++ src/main/java/com/nemo/sj/spring/config/MyTypeFiler.java
@@ -0,0 +1,45 @@
+package com.nemo.sj.spring.config;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
+
+import java.io.IOException;
+
+/**
+ * create by Nemo
+ * 2018/9/17  11:06
+ */
+public class MyTypeFiler implements TypeFilter {
+    /**
+     *
+     * @param metadataReader   读取到的当前正在扫描的类的信息
+     * @param metadataReaderFactory     factory for obtaining metadata readers
+     * 	 * for other classes (such as superclasses and interfaces)
+     *
+     * @return
+     * @throws IOException
+     */
+    public boolean match(MetadataReader metadataReader, MetadataReaderFactory metadataReaderFactory) throws IOException {
+
+        // 获取当前类注解的信息
+        AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
+
+
+        // 获取当前正在扫描的类信息
+        ClassMetadata classMetadata = metadataReader.getClassMetadata();
+
+        // 获取当前类资源（类的路径）
+        Resource resource = metadataReader.getResource();
+
+
+        String className = classMetadata.getClassName();
+        System.out.println("--->"+className);
+        if (className.equalsIgnoreCase("com.nemo.sj.spring.controller.BookController"))return true;
+
+        return false;
+    }
+}
